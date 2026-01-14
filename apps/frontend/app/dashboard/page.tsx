474d7d5c5115247dd211d33b9530bae739921145
yash tiwari <yashtiwari700714@gmail.com>
2026-01-15 03:36:51 +0530

    frontend : create footer & seperate dashboard page

---
+++ apps/frontend/app/dashboard/page.tsx
@@ -0,0 +1,21 @@
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import Dashboard from "@/components/dashboard";
+
+export default async function DashboardPage() {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("auth_token")?.value;
+
+    if (!token) {
+        redirect("/login");
+    }
+
+    try {
+        jwt.verify(token, process.env.NEXT_PUBLIC_JWT_SECRET!);
+    } catch {
+        redirect("/login");
+    }
+
+    return <Dashboard />;
+}
--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,21 +1,35 @@
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
-import Dashboard from "@/components/dashboard";
+import Header from "@/components/header";
+import Hero from "@/components/hero";
+import GridCard from "@/components/GridCard";
+import PricingSection from "@/components/SubscriptionPlans";
+import Footer from "@/components/Footer";
 
-export default async function DashboardPage() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get("auth_token")?.value;
+export default async function Page() {
+  // const cookieStore = await cookies();
+  // const token = cookieStore.get("auth_token")?.value;
 
-  if (!token) {
-    redirect("/login");
-  }
+  // if (!token) {
+  //   redirect("/login");
+  // }
 
-  try {
-    jwt.verify(token, process.env.NEXT_PUBLIC_JWT_SECRET!);
-  } catch {
-    redirect("/login");
-  }
+  // try {
+  //   jwt.verify(token, process.env.NEXT_PUBLIC_JWT_SECRET!);
+  // } catch {
+  //   redirect("/login");
+  // }
 
-  return <Dashboard />;
+  return (
+    <>
+        <Header />
+        <main>
+            <Hero />
+            <GridCard />
+            <PricingSection />
+        </main>
+        <Footer />
+    </>
+  );
 }

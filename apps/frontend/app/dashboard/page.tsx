d011e7bcd0c3b61d89a25328080832ee625d853b
yash tiwari <yashtiwari700714@gmail.com>
2026-01-16 16:52:51 +0530

    frontend : add middleware

--- apps/frontend/app/dashboard/page.tsx
+++ apps/frontend/app/dashboard/page.tsx
@@ -1,31 +1,11 @@
-// import { redirect } from "next/navigation";
-// import { cookies } from "next/headers";
-// import jwt from "jsonwebtoken";
-// import Dashboard from "@/components/dashboard";
-
-// export default async function DashboardPage() {
-//     const cookieStore = await cookies();
-//     const token = cookieStore.get("auth_token")?.value;
-
-//     if (!token) {
-//         redirect("/signin");
-//     }
-
-//     try {
-//         jwt.verify(token, process.env.NEXT_PUBLIC_JWT_SECRET!);
-//     } catch {
-//         redirect("/signin");
-//     }
-
-//     return <Dashboard />;
-// }
+"use client";
 
 export default function DashboardHome() {
-  return (
-    <div>
-      <h1 className="text-2xl font-semibold">Dashboard</h1>
-      <p className="mt-2 text-sm text-gray-600">Welcome to Verity AI dashboard.</p>
-    </div>
-  );
+    return (
+        <div>
+        <h1 className="text-2xl font-semibold">Dashboard</h1>
+        <p className="mt-2 text-sm text-gray-600">Welcome to Verity AI dashboard.</p>
+        </div>
+    );
 }
 
